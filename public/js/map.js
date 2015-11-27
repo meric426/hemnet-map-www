1c03b6c9167e5d76d5efd339fdb4c99ae95f1555
Patrik Wibron <patrik@wibron.nu>
2015-11-27 11:28:14 +0100

    Move JS logic to a file

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,19 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicGF0cmlrd2licm9uIiwiYSI6ImNpaGhld3JidDAwOXZ2a205Y3puems0MG8ifQ.fRIZxQrmaWG45atGisIrGQ';
+var socket = io('192.168.32.112:4000');
 
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v8',
-    center: [18.065818, 59.338675], // starting position
-    zoom: 5 // starting zoom
+L.mapbox.accessToken = 'pk.eyJ1IjoicGF0cmlrd2licm9uIiwiYSI6ImNpaGhldnJteDAwYTR2dGtxOHZkdWpsY2oifQ.RHt6xn90DI072UEXAEzhWQ';
+var map = L.mapbox.map('map', 'mapbox.dark')
+    .setView([61.653860, 16.503246], 6);
+
+var customIcon = L.divIcon({
+  className: 'property__icon',
+  iconSize: [10, 10]
+});
+
+socket.on('coords', function (coord) {
+  var marker = L.marker([coord.lat, coord.lng], { icon: customIcon});
+  marker.addTo(map);
+
+  setTimeout(function () {
+    map.removeLayer(marker);
+  }, 1000 * 3);
 });
