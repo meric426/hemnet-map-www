8ba8e9b7fd70430504955c22d7040a111f70a3fe
Patrik Wibron <patrik@wibron.nu>
2015-11-27 15:21:21 +0100

    Add gulp and gulp-sass

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch('public/css/*.scss', ['sass']);
+    gulp.watch('*.html, public/js/**/*.js').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src("public/css/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("public/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
