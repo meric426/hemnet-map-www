bb9146c7dcfd7f54120c07974f6097d2ae68590b
Patrik Wibron <patrik@wibron.nu>
2016-04-28 11:45:43 +0200

    Display a plain GL map without coords

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import { accessToken, socketHost } from './config';
 import io from 'socket.io-client';
+import mapboxgl from 'mapbox-gl';
 
 const socket = io(socketHost);
 
-L.mapbox.accessToken = accessToken;
-const map = L.mapbox.map('map', 'mapbox.dark')
-    .setView([61.653860, 16.503246], 6);
+mapboxgl.accessToken = accessToken;
 
-const createIcon = (coords) => {
-  return L.divIcon({
-    className: `property__icon property__icon--${getMarkerType(coords)}`,
-    iconSize: [10, 10]
-  })
-};
-
-// Returns web / mobile
-const getMarkerType = (coords) => {
-  return coords.type.split('.').shift();
-};
-
-socket.on('coords', (coord) => {
-  const marker = L.marker([coord.lat, coord.lng], { icon: createIcon(coord) });
-  marker.addTo(map);
-
-  setTimeout(() => {
-    map.removeLayer(marker);
-  }, 1000 * 2);
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v8',
+  center: [16.503246, 61.653860],
+  zoom: 5,
 });
