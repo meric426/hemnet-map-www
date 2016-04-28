64b55404cbaf5c564ae31a192f6007efcfdf3a92
Patrik Wibron <patrik@wibron.nu>
2016-04-28 11:04:06 +0200

    Refactor and add comment
    Also remove unused method

--- src/index.js
+++ src/index.js
@@ -7,24 +7,20 @@ L.mapbox.accessToken = accessToken;
 const map = L.mapbox.map('map', 'mapbox.dark')
     .setView([61.653860, 16.503246], 6);
 
-const customIcon = L.divIcon({
-  className: 'property__icon',
-  iconSize: [10, 10]
-});
-
-const setStyles = (coords) => {
+const createIcon = (coords) => {
   return L.divIcon({
-    className: 'property__icon ' + 'property__icon--' + getMarkerType(coords),
+    className: `property__icon property__icon--${getMarkerType(coords)}`,
     iconSize: [10, 10]
   })
 };
 
+// Returns web / mobile
 const getMarkerType = (coords) => {
   return coords.type.split('.').shift();
 };
 
 socket.on('coords', (coord) => {
-  const marker = L.marker([coord.lat, coord.lng], { icon: setStyles(coord) });
+  const marker = L.marker([coord.lat, coord.lng], { icon: createIcon(coord) });
   marker.addTo(map);
 
   setTimeout(() => {
