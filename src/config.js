1e381928368b083164935e13b242225c3823e2ea
Patrik Wibron <patrik@wibron.nu>
2016-12-07 23:11:44 +0100

    Don't hard code to an IP address

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   accessToken: 'pk.eyJ1IjoicGF0cmlrd2licm9uIiwiYSI6ImNpaGhldnJteDAwYTR2dGtxOHZkdWpsY2oifQ.RHt6xn90DI072UEXAEzhWQ',
-  socketHost: '192.168.33.93:4000',
+  socketHost: '127.0.0.1:4000',
 }
