3162617a1e108cc6539c3f19f566160e4810947e
art1373 <alireza.tavakol@hemnet.se>
2023-10-26 10:41:14 +0200

    added auto bundle for sass, and sass watcher in dev mode

--- build.js
+++ build.js
@@ -1,7 +1,14 @@
-var fs = require("fs");
-var browserify = require("browserify");
+const fs = require("fs");
+const browserify = require("browserify");
+const sass = require("sass");
 
 browserify("./src/index.js")
-  .transform("babelify", {presets: ["@babel/preset-env"]})
+  .transform("babelify", { presets: ["@babel/preset-env"] })
   .bundle()
   .pipe(fs.createWriteStream("dist/bundle.js"));
+
+const result = sass.compile("./src/style.scss");
+fs.writeFile("./dist/style.css", result.css, (err) => {
+  if (err) throw err;
+  console.log("The CSS has been saved to style.css!");
+});
